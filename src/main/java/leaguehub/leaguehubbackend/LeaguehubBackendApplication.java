c065f1971cf570f7e87cca03edeb218d0f6538f8
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-05-21 15:55:35 +0900

    config: initial config

---
+++ src/main/java/leaguehub/leaguehubbackend/LeaguehubBackendApplication.java
@@ -0,0 +1,13 @@
+package leaguehub.leaguehubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaguehubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaguehubBackendApplication.class, args);
+    }
+
+}
