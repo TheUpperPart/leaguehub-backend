50ed6591239f0e307431cb7ddeb4c4183e715621
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-06-10 18:42:25 +0900

    Test: 호스트 참가자 생성 테스트

--- src/main/java/leaguehub/leaguehubbackend/repository/particiapnt/ParticipantRepository.java
+++ src/main/java/leaguehub/leaguehubbackend/repository/particiapnt/ParticipantRepository.java
@@ -4,4 +4,6 @@ import leaguehub.leaguehubbackend.entity.participant.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+
+    Participant findParticipantById(Long id);
 }
--- src/test/java/leaguehub/leaguehubbackend/entity/participant/ParticipantTest.java
+++ src/test/java/leaguehub/leaguehubbackend/entity/participant/ParticipantTest.java
@@ -1,11 +1,13 @@
 package leaguehub.leaguehubbackend.entity.participant;
 
-import leaguehub.leaguehubbackend.dto.channel.CreateChannelDto;
 import leaguehub.leaguehubbackend.entity.channel.Channel;
 import leaguehub.leaguehubbackend.entity.constant.GlobalConstant;
 import leaguehub.leaguehubbackend.entity.member.Member;
+import leaguehub.leaguehubbackend.fixture.ChannelFixture;
+import leaguehub.leaguehubbackend.fixture.UserFixture;
 import leaguehub.leaguehubbackend.repository.channel.ChannelRepository;
 import leaguehub.leaguehubbackend.repository.member.MemberRepository;
+import leaguehub.leaguehubbackend.repository.particiapnt.ParticipantRepository;
 import leaguehub.leaguehubbackend.service.ChannelService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +17,6 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,46 +29,30 @@ class ParticipantTest {
     @Autowired
     ChannelService channelService;
     @Autowired
-    private ChannelRepository channelRepository;
+    ChannelRepository channelRepository;
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    ParticipantRepository participantRepository;
 
-    Member createMember() {
-        Member member = Member.builder()
-                .personalId("id").profileImageUrl("url")
-                .nickname("test").build();
-
-        return member;
-    }
-
-    CreateChannelDto createChannelDto() {
-        CreateChannelDto createChannelDto = CreateChannelDto.builder()
-                .game(0)
-                .title("test")
-                .tournament(0)
-                .participationNum(16)
-                .tier(false)
-                .playCount(false)
-        .build();
-
-
-        return createChannelDto;
-    }
 
     @Test
     @DisplayName("채널 생성시 유저는 호스트 권한을 가진 참가자가 된다.")
     public void createHostTest() throws Exception {
 
-        Member member = memberRepository.save(createMember());
+        Member member = memberRepository.save(UserFixture.createMember());
 
-        Long id = channelService.createChannel(createChannelDto(), "id");
-        Optional<Channel> findChannel = channelRepository.findById(id);
-        List<Participant> participants = findChannel.get().getParticipant();
+        Channel channel = Channel.createChannel(ChannelFixture.createChannelDto(), member);
+        Channel savedChannel = channelRepository.save(channel);
 
-        Participant participant = participants.get(0);
+        List<Participant> participantList = savedChannel.getParticipant();
+        Participant participant = participantList.get(0);
+        Participant findParticipant = participantRepository.findParticipantById(participant.getId());
 
 
-        assertThat(participants.size()).isEqualTo(1);
+        assertThat(participantList.size()).isEqualTo(1);
+        assertThat(findParticipant.getId()).isEqualTo(participant.getId());
+        assertThat(findParticipant.getMember()).isEqualTo(member);
         assertThat(participant.getMember()).isEqualTo(member);
         assertThat(participant.getRole()).isEqualTo(Role.HOST);
         assertThat(participant.getGameId()).isEqualTo(GlobalConstant.NO_DATA.getData());
