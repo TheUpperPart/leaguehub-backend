80c50f9313aa3e3d6b22997f0b68ada1914b35af
ghdtjddn <sungwoo166@gmail.com>
2023-06-11 23:31:15 +0900

    Feat : MemberRepository 구현

---
+++ src/main/java/leaguehub/leaguehubbackend/repository/member/MemberRepository.java
@@ -0,0 +1,14 @@
+package leaguehub.leaguehubbackend.repository.member;
+
+import leaguehub.leaguehubbackend.entity.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+
+    Optional<Member> findMemberByPersonalId(String personalId);
+    Optional<Member> findByNickname(String nickname);
+    Optional<Member> findByRefreshToken(String refreshToken);
+}
