fa4d8196d67a122cc1856198809741bd450664d0
SungWoo <sungwoo166@gmail.com>
2023-06-20 00:12:34 +0900

    Feat : 회원가입을 위한 Memberservice 구현

---
+++ src/main/java/leaguehub/leaguehubbackend/service/member/MemberService.java
@@ -0,0 +1,33 @@
+package leaguehub.leaguehubbackend.service.member;
+
+
+import jakarta.transaction.Transactional;
+import leaguehub.leaguehubbackend.dto.kakao.KakaoUserDto;
+import leaguehub.leaguehubbackend.entity.member.Member;
+import leaguehub.leaguehubbackend.repository.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Optional<Member> findMemberByPersonalId(String personalId) {
+        return memberRepository.findMemberByPersonalId(personalId);
+    }
+
+    public Optional<Member> findMemberByRefreshToken(String refreshToken) {
+        return memberRepository.findByRefreshToken(refreshToken);
+    }
+
+    @Transactional
+    public Optional<Member> saveMember(KakaoUserDto kakaoUserDto) {
+        Member newUser = Member.kakaoUserToMember(kakaoUserDto);
+        memberRepository.save(newUser);
+        return Optional.of(newUser);
+    }
+}
