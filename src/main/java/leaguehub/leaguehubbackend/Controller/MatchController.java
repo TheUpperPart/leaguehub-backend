734a032c8af921c02763bf6d459e8af8f919e6d7
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-06-26 17:54:18 +0900

    Feat: 경기 결과 조회 및 저장 로직 구현

---
+++ src/main/java/leaguehub/leaguehubbackend/Controller/MatchController.java
@@ -0,0 +1,33 @@
+package leaguehub.leaguehubbackend.Controller;
+
+import leaguehub.leaguehubbackend.dto.match.MatchRankResultDto;
+import leaguehub.leaguehubbackend.dto.match.MatchResponseDto;
+import leaguehub.leaguehubbackend.service.match.MatchRankService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class MatchController {
+
+    private final MatchRankService matchRankService;
+
+    @PostMapping("/match/matchResult")
+    public ResponseEntity createMatchRank(@RequestBody MatchResponseDto matchResponseDto){
+
+        matchRankService.setMatchDetail(matchResponseDto);
+
+        return new ResponseEntity<>("매치 결과가 생성되었습니다.", HttpStatus.CREATED);
+    }
+
+    @GetMapping("/match/matchRank")
+    public List<MatchRankResultDto> matchRankResult(String matchId){
+
+        return matchRankService.getMatchDetail(matchId);
+    }
+}
---
+++ src/main/java/leaguehub/leaguehubbackend/service/match/MatchRankService.java
@@ -0,0 +1,167 @@
+package leaguehub.leaguehubbackend.service.match;
+
+import leaguehub.leaguehubbackend.dto.match.MatchRankResultDto;
+import leaguehub.leaguehubbackend.dto.match.MatchResponseDto;
+import leaguehub.leaguehubbackend.entity.match.Match;
+import leaguehub.leaguehubbackend.entity.match.MatchRank;
+import leaguehub.leaguehubbackend.entity.match.MatchResult;
+import leaguehub.leaguehubbackend.exception.global.exception.GlobalServerErrorException;
+import leaguehub.leaguehubbackend.exception.match.exception.MatchResultIdNotFoundException;
+import leaguehub.leaguehubbackend.repository.match.MatchRankRepository;
+import leaguehub.leaguehubbackend.repository.match.MatchRepository;
+import leaguehub.leaguehubbackend.repository.match.MatchResultRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class MatchRankService {
+
+    @Value("${riot-api-key-1}")
+    private String riot_api_key_1;
+    @Value("${riot-api-key-2}")
+    private String riot_api_key_2;
+
+    private final MatchRepository matchRepository;
+    private final MatchRankRepository matchRankRepository;
+    private final MatchResultRepository matchResultRepository;
+
+    /**
+     *
+     * @param name 게임 닉네임
+     * @return puuid
+     */
+    public String getSummonerPuuid(String name) {
+        String summonerUrl = "https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-name/";
+
+        WebClient webClient = WebClient.create();
+
+        JSONObject summonerDetail = webClient.get()
+                .uri(summonerUrl + name + riot_api_key_1)
+                .retrieve()
+                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new GlobalServerErrorException()))
+                .bodyToMono(JSONObject.class)
+                .block();
+
+        String puuid = Objects.requireNonNull(summonerDetail).get("puuid").toString();
+
+        return puuid;
+    }
+
+    /**
+     * puuid로 매치 id 검색
+     * @param puuid
+     * @return
+     */
+    public String getMatch(String puuid) {
+        long endTime = System.currentTimeMillis() / 1000;
+        long statTime = 0;
+
+        String matchUrl = "https://asia.api.riotgames.com/tft/match/v1/matches/by-puuid/";
+        String Option = "/ids?start=0&endTime=" + endTime + "&startTime=" + statTime + "&count=1";
+
+
+        WebClient webClient = WebClient.create();
+
+        JSONArray matchArray = webClient.get()
+                .uri(matchUrl + puuid + Option + riot_api_key_2)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, response -> Mono.error(new MatchResultIdNotFoundException()))
+                .onStatus(HttpStatusCode::is5xxServerError, response -> Mono.error(new GlobalServerErrorException()))
+                .bodyToMono(JSONArray.class)
+                .block();
+
+
+        return matchArray.get(0).toString();
+
+    }
+
+
+    /**
+     * 최근 매치 id로 경기 세부사항 추출
+     * @param matchResponseDto
+     * @return matchRankResultDto
+     */
+    @SneakyThrows
+    public void setMatchDetail(MatchResponseDto matchResponseDto) {
+        String matchDetailUrl = "https://asia.api.riotgames.com/tft/match/v1/matches/";
+
+        String puuid = getSummonerPuuid(matchResponseDto.getNickName());
+        String matchId = getMatch(puuid);
+
+        JSONParser jsonParser = new JSONParser();
+        WebClient webClient = WebClient.create();
+
+        JSONObject matchDetailJSON = (JSONObject) jsonParser.parse
+                (webClient
+                        .get()
+                        .uri(matchDetailUrl + matchId + riot_api_key_1)
+                        .retrieve()
+                        .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new GlobalServerErrorException()))
+                        .bodyToMono(JSONObject.class)
+                        .block().toJSONString());
+
+
+        Match match = matchRepository.findByMatchNameAndMatchPasswd(
+                matchResponseDto.getMatchName(),
+                matchResponseDto.getMatchPasswd());
+        MatchResult matchResult = MatchResult.createMatchResult(matchId, match);
+        matchResultRepository.save(matchResult);
+
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(matchDetailJSON.get("info").toString());
+        JSONArray jsonArray = (JSONArray) jsonParser.parse(jsonObject.get("participants").toString());
+        for (int i = 0; i < 8; i++) {
+            JSONObject participants = (JSONObject) jsonParser.parse(jsonArray.get(i).toString());
+            String placement = participants.get("placement").toString();
+            String parti1puuid = participants.get("puuid").toString();
+
+
+            String summonerName = webClient.get()
+                    .uri("https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/" + parti1puuid + riot_api_key_1)
+                    .retrieve()
+                    .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> Mono.error(new GlobalServerErrorException()))
+                    .bodyToMono(JSONObject.class)
+                    .block().get("name").toString();
+
+
+            MatchRank matchRank = MatchRank.createMatchRank(summonerName, placement, matchResult);
+            matchRankRepository.save(matchRank);
+
+        }
+
+    }
+
+    /**
+     * 해당 경기 순위 반환
+     * @param matchId
+     * @return MatchRankResultDto
+     */
+    public List<MatchRankResultDto> getMatchDetail(String matchId){
+
+        List<MatchRankResultDto> dtoList = new ArrayList<>();
+
+        Long id = matchResultRepository.findByMatchCode(matchId).getId();
+
+        List<MatchRank> list = matchRankRepository.findByMatchResult_Id(id);
+
+        for(MatchRank matchRank : list){
+            MatchRankResultDto dto = new MatchRankResultDto();
+            dto.setName(matchRank.getParticipant());
+            dto.setPlacement(matchRank.getPlacement());
+            dtoList.add(dto);
+        }
+        return dtoList;
+    }
+}
