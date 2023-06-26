a2230e37d2adcabf52373174422a51cfc26dcd1d
SungWoo <sungwoo166@gmail.com>
2023-06-26 16:43:32 +0900

    Refactor : KakaoController에 RequestMapping 추가

--- src/main/java/leaguehub/leaguehubbackend/controller/KakaoController.java
+++ src/main/java/leaguehub/leaguehubbackend/controller/KakaoController.java
@@ -14,6 +14,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -22,6 +23,7 @@ import java.util.function.Predicate;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class KakaoController {
 
     private final KakaoService kakaoService;
