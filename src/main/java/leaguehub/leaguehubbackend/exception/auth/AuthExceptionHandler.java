9f7f751d65299c8904b43d06a2fe1250965b7386
SungWoo <sungwoo166@gmail.com>
2023-06-28 21:12:03 +0900

    Feat : auth 관련 exception 구현

--- src/main/java/leaguehub/leaguehubbackend/exception/auth/AuthExceptionHandler.java
+++ src/main/java/leaguehub/leaguehubbackend/exception/auth/AuthExceptionHandler.java
@@ -1,23 +1,34 @@
 package leaguehub.leaguehubbackend.exception.auth;
 
+import leaguehub.leaguehubbackend.exception.auth.exception.AuthExpiredTokenException;
 import leaguehub.leaguehubbackend.exception.auth.exception.AuthInvalidTokenException;
 import leaguehub.leaguehubbackend.exception.global.ExceptionCode;
 import leaguehub.leaguehubbackend.exception.global.ExceptionResponse;
-import leaguehub.leaguehubbackend.exception.kakao.exception.KakaoInvalidCodeException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
-@ControllerAdvice
 @RequiredArgsConstructor
 public class AuthExceptionHandler {
 
     @ExceptionHandler(AuthInvalidTokenException.class)
     public ResponseEntity<ExceptionResponse> authInvalidTokenException(
             AuthInvalidTokenException e
+    ) {
+        ExceptionCode exceptionCode = e.getExceptionCode();
+        log.error("{}", exceptionCode.getMessage());
+
+        return new ResponseEntity<>(
+                new ExceptionResponse(exceptionCode),
+                exceptionCode.getHttpStatus()
+        );
+    }
+
+    @ExceptionHandler(AuthExpiredTokenException.class)
+    public ResponseEntity<ExceptionResponse> authExpiredTokenException(
+            AuthExpiredTokenException e
     ) {
         ExceptionCode exceptionCode = e.getExceptionCode();
         log.error("{}", exceptionCode.getMessage());
