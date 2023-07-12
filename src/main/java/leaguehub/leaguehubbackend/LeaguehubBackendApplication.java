34db3274729512b08fc66f1a84411c0eb6104102
SungWoo <sungwoo166@gmail.com>
2023-07-12 11:05:22 +0900

    Feat : 앱 시작시 회원등록

--- src/main/java/leaguehub/leaguehubbackend/LeaguehubBackendApplication.java
+++ src/main/java/leaguehub/leaguehubbackend/LeaguehubBackendApplication.java
@@ -1,13 +1,26 @@
 package leaguehub.leaguehubbackend;
 
+import jakarta.annotation.PostConstruct;
+import leaguehub.leaguehubbackend.util.UserUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LeaguehubBackendApplication {
+    private final UserUtil userUtil;
+
+    @Autowired
+    public LeaguehubBackendApplication(UserUtil userUtil) {
+        this.userUtil = userUtil;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LeaguehubBackendApplication.class, args);
     }
 
+    @PostConstruct
+    protected void init() {
+        userUtil.addDefaultUsers();
+    }
 }
