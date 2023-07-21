2a807754a089268e653a8e1de605c6ee7c0a33e5
SungWoo <sungwoo166@gmail.com>
2023-07-22 01:27:08 +0900

    Feat : Swagger 문서에 설명추가, ApiResponse 추가

--- src/main/java/leaguehub/leaguehubbackend/Controller/MemberController.java
+++ src/main/java/leaguehub/leaguehubbackend/Controller/MemberController.java
@@ -1,8 +1,18 @@
 package leaguehub.leaguehubbackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import leaguehub.leaguehubbackend.dto.member.LoginMemberResponse;
 import leaguehub.leaguehubbackend.dto.member.ProfileResponseDto;
+import leaguehub.leaguehubbackend.exception.global.ExceptionResponse;
 import leaguehub.leaguehubbackend.service.member.MemberService;
 import leaguehub.leaguehubbackend.util.SecurityUtils;
 import lombok.RequiredArgsConstructor;
@@ -18,10 +28,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-public class MemberController {
+@Tag(name = "Member-Controller", description = "사용자 API")
+public class MemberController  {
 
     private final MemberService memberService;
 
+    @Operation(summary = "사용자 프로필 조회", description = "사용자 id, 이름, 이미지 url 조회")
+    @Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "Bearer {Access-Token}", required = true)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "사용자 프로필 조회 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProfileResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "MB-C-001 존재하지 않는 회원입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))),
+    })
     @GetMapping("/profile")
     public ProfileResponseDto getProfile() {
 
@@ -30,6 +47,12 @@ public class MemberController {
         return memberService.getMemberProfile(userDetails.getUsername());
     }
 
+    @Operation(summary = "앱 로그아웃", description = "앱에서 사용자를 로그아웃")
+    @Parameter(in = ParameterIn.HEADER, name = "Authorization", description = "Bearer {Access-Token}", required = true)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그아웃 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "404", description = "MB-C-001 존재하지 않는 회원입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))),
+    })
     @PostMapping("/app/logout")
     public ResponseEntity<String> handleKakaoLogout(HttpServletRequest request, HttpServletResponse response) {
         UserDetails userDetails = SecurityUtils.getAuthenticatedUser();
--- src/main/java/leaguehub/leaguehubbackend/controller/JwtController.java
+++ src/main/java/leaguehub/leaguehubbackend/controller/JwtController.java
@@ -1,11 +1,22 @@
 package leaguehub.leaguehubbackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import leaguehub.leaguehubbackend.dto.member.LoginMemberResponse;
 import leaguehub.leaguehubbackend.entity.member.Member;
 import leaguehub.leaguehubbackend.exception.auth.exception.AuthInvalidRefreshToken;
 import leaguehub.leaguehubbackend.exception.auth.exception.AuthInvalidTokenException;
 import leaguehub.leaguehubbackend.exception.auth.exception.AuthTokenNotFoundException;
+import leaguehub.leaguehubbackend.exception.global.ExceptionResponse;
 import leaguehub.leaguehubbackend.service.jwt.JwtService;
 import leaguehub.leaguehubbackend.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -21,12 +32,22 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Tag(name = "Jwt-Controller", description = "JWT 토큰 API")
 public class JwtController {
 
     private final MemberService memberService;
 
     private final JwtService jwtService;
 
+    @Operation(summary = "토큰 재발급", description = "refreshToken을 사용해서 accessToken 과 refreshToken 재발급")
+    @Parameter(in = ParameterIn.HEADER, name = "Authorization-refresh", description = "Bearer {refresh-token}", required = true)
+    @SecurityRequirements
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "토큰 재발급 성공", content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginMemberResponse.class))),
+            @ApiResponse(responseCode = "400_1", description = "AT-C-004 요청에 토큰이 존재하지 않습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))),
+            @ApiResponse(responseCode = "400_2", description = "AT-C-005 해당 리프레쉬 토큰을 가지는 멤버가 없습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class))),
+            @ApiResponse(responseCode = "401", description = "AT-C-001 유효하지 않은 토큰입니다.", content = @Content(mediaType = "application/json",schema = @Schema(implementation = ExceptionResponse.class))),
+    })
     @GetMapping("/reissue/token")
     public ResponseEntity<LoginMemberResponse> refreshAccessToken(HttpServletRequest request) {
 
