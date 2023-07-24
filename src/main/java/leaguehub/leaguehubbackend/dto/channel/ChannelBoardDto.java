0d3de56abe73498ed46c3c57aba6cecd2f9dc6c5
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-07-24 16:58:59 +0900

    Fix: Dto 컨버터 이슈 해결
    기본 생성자 추가

--- src/main/java/leaguehub/leaguehubbackend/dto/channel/ChannelBoardDto.java
+++ src/main/java/leaguehub/leaguehubbackend/dto/channel/ChannelBoardDto.java
@@ -1,8 +1,10 @@
 package leaguehub.leaguehubbackend.dto.channel;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class ChannelBoardDto {
 
     private String title;
