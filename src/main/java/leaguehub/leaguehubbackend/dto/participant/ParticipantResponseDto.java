72a77de3ff0b4e0f7b58b63552b811e2629f3a59
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-08-01 18:01:38 +0900

    Docs: 사용되는 Dto에 Swagger 어노테이션 추가

--- src/main/java/leaguehub/leaguehubbackend/dto/participant/ParticipantResponseDto.java
+++ src/main/java/leaguehub/leaguehubbackend/dto/participant/ParticipantResponseDto.java
@@ -1,14 +1,18 @@
 package leaguehub.leaguehubbackend.dto.participant;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class ParticipantResponseDto {
 
+    @Schema(example = "42aa1b11ab88")
     String channelLink;
 
+    @Schema(example = "칸영기")
     String gameId;
 
+    @Schema(example = "채널개인닉네임")
     String nickname;
 }
