51f4af666d0dc39490fbffa2c98e2f567a61dcc8
SungWoo <sungwoo166@gmail.com>
2023-08-02 21:09:44 +0900

    Feat : EmailConfig 구현

---
+++ src/main/java/leaguehub/leaguehubbackend/config/EmailConfig.java
@@ -0,0 +1,42 @@
+package leaguehub.leaguehubbackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Value("${LEAGUE_HUB_EMAIL}")
+    private String emailAddress;
+
+    @Value("${LEAGUE_HUB_EMAIL_PW}")
+    private String emailPassword;
+
+    @Bean
+    public JavaMailSender mailSender() {
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(emailAddress);
+        mailSender.setPassword(emailPassword);
+
+        Properties javaMailProperties = new Properties();
+        javaMailProperties.put("mail.transport.protocol", "smtp");
+        javaMailProperties.put("mail.smtp.auth", "true");
+        javaMailProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        javaMailProperties.put("mail.smtp.starttls.enable", "true");
+        javaMailProperties.put("mail.debug", "true");
+        javaMailProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        javaMailProperties.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+        mailSender.setJavaMailProperties(javaMailProperties);
+
+        return mailSender;
+    }
+}
