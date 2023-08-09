05ea502a91439c1314cdc6992805ccf4b1ea38ee
김현준 <102659136+HyeonJun0530@users.noreply.github.com>
2023-08-09 17:11:17 +0900

    docs: Swagger UI 어노테이션 추가 (#125)

--- src/main/java/leaguehub/leaguehubbackend/Controller/channel/ChannelBoardController.java
+++ src/main/java/leaguehub/leaguehubbackend/Controller/channel/ChannelBoardController.java
@@ -1,7 +1,14 @@
 package leaguehub.leaguehubbackend.controller.channel;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import leaguehub.leaguehubbackend.dto.channel.ChannelBoardDto;
 import leaguehub.leaguehubbackend.dto.channel.ChannelBoardLoadDto;
+import leaguehub.leaguehubbackend.dto.channel.ResponseChannelDto;
+import leaguehub.leaguehubbackend.exception.global.ExceptionResponse;
 import leaguehub.leaguehubbackend.service.channel.ChannelBoardService;
 import leaguehub.leaguehubbackend.service.channel.ChannelService;
 import leaguehub.leaguehubbackend.service.participant.ParticipantService;
@@ -22,6 +29,11 @@ public class ChannelBoardController {
 
     private final ChannelBoardService channelBoardService;
 
+    @Operation(summary = "채널 보드 만들기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ChannelBoardLoadDto.class))),
+            @ApiResponse(responseCode = "400", description = "채널 링크가 올바르지 않음, 관리자 권한 없음", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class)))
+    })
     @PostMapping("/channel/{channelLink}/new")
     public ResponseEntity createChannelBoard(@PathVariable("channelLink") String channelLink,
                                              @RequestBody ChannelBoardDto request) {
@@ -30,6 +42,11 @@ public class ChannelBoardController {
         return new ResponseEntity(channelBoardLoadDto, OK);
     }
 
+    @Operation(summary = "채널 보드 업데이트")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "채널 링크가 올바르지 않음, 관리자 권한 없음", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class)))
+    })
     @PostMapping("/channel/{channelLink}/{boardId}")
     public ResponseEntity updateChannelBoard(@PathVariable("channelLink") String channelLink,
                                           @PathVariable("boardId") Long boardId,
@@ -39,6 +56,11 @@ public class ChannelBoardController {
         return new ResponseEntity("Board successfully updated", OK);
     }
 
+    @Operation(summary = "채널 보드 삭제")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "채널 링크가 올바르지 않음, 관리자 권한 없음", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class)))
+    })
     @DeleteMapping("/channel/{channelLink}/{boardId}")
     public ResponseEntity deleteChannelBoard(@PathVariable("channelLink") String channelLink,
                                              @PathVariable("boardId") Long boardId) {
@@ -48,6 +70,11 @@ public class ChannelBoardController {
         return new ResponseEntity("Board successfully deleted", OK);
     }
 
+    @Operation(summary = "채널 보드 가져오기 - 단일 채널 보드 읽기")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ChannelBoardDto.class))),
+            @ApiResponse(responseCode = "400", description = "채널 링크가 올바르지 않음", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class)))
+    })
     @GetMapping("/channel/{channelLink}/{boardId}")
     public ResponseEntity getChannelBoard(@PathVariable("channelLink") String channelLink,
                                           @PathVariable("boardId") Long boardId) {
@@ -56,6 +83,11 @@ public class ChannelBoardController {
         return new ResponseEntity(channelBoardDto, OK);
     }
 
+    @Operation(summary = "채널 보드 인덱스 업데이트 - 채널 보드 인덱스 커스텀")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "채널 링크가 올바르지 않음, 권한x", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class)))
+    })
     @PostMapping("/channel/{channelLink}/index")
     public ResponseEntity updateChannelBoardIndex(@PathVariable("channelLink") String channelLink,
                                                   @RequestBody List<ChannelBoardLoadDto> channelBoardLoadDtoList) {
@@ -64,6 +96,11 @@ public class ChannelBoardController {
         return new ResponseEntity("BoardIndex successfully updated", OK);
     }
 
+    @Operation(summary = "채널 보드 가져오기 - 채널 로드시 제목과 보드ID 가져오기 리스트로 반환")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ChannelBoardLoadDto.class))),
+            @ApiResponse(responseCode = "400", description = "채널 링크가 올바르지 않음", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class)))
+    })
     @GetMapping("/channel/{channelLink}/boards")
     public ResponseEntity loadChannelBoards(@PathVariable("channelLink") String channelLink) {
 
