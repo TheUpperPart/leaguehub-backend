e4f4517ff6fbc8b6beb003dfe5fbef47f712535b
SungWoo <sungwoo166@gmail.com>
2023-08-10 22:44:37 +0900

    Feat : 응답 dto 구현

---
+++ src/main/java/leaguehub/leaguehubbackend/dto/member/MypageResponseDto.java
@@ -0,0 +1,17 @@
+package leaguehub.leaguehubbackend.dto.member;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MypageResponseDto {
+
+    private String profileImageUrl;
+
+    private String nickName;
+
+    private String email;
+
+    private boolean userEmailVerified;
+}
