eb3b9ae44dbddfd5418c86fd2e850d3d8059118f
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-08-13 17:06:26 +0900

    refactor: import 최적화

--- src/main/java/leaguehub/leaguehubbackend/dto/email/EmailDto.java
+++ src/main/java/leaguehub/leaguehubbackend/dto/email/EmailDto.java
@@ -1,10 +1,10 @@
 package leaguehub.leaguehubbackend.dto.email;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import jakarta.validation.constraints.NotBlank;
 
 
 @Data
--- src/main/java/leaguehub/leaguehubbackend/dto/participant/ParticipantDto.java
+++ src/main/java/leaguehub/leaguehubbackend/dto/participant/ParticipantDto.java
@@ -3,7 +3,6 @@ package leaguehub.leaguehubbackend.dto.participant;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
