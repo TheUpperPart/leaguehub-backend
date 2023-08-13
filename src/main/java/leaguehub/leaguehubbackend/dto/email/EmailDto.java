17d34427e49e8fbbbfa734590bca943e95c95fd4
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-08-13 17:04:57 +0900

    henni 님이 변경한 내용

--- src/main/java/leaguehub/leaguehubbackend/dto/email/EmailDto.java
+++ src/main/java/leaguehub/leaguehubbackend/dto/email/EmailDto.java
@@ -4,12 +4,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmailDto {
 
+    @NotBlank
     @Schema(description = "이메일 주소", example = "test@naver.com")
     private String email;
 
