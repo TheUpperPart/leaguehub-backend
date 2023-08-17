c077e01a80a14f4be4b7d311f7f2c5b70d43a05c
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-08-17 19:16:53 +0900

    Feat: 참가자 배정 및 조회 기능 구현

--- src/main/java/leaguehub/leaguehubbackend/repository/match/MatchPlayerRepository.java
+++ src/main/java/leaguehub/leaguehubbackend/repository/match/MatchPlayerRepository.java
@@ -8,4 +8,8 @@ import java.util.List;
 public interface MatchPlayerRepository extends JpaRepository<MatchPlayer, Long> {
 
     List<MatchPlayer> findAllByMatch_MatchLinkAndMatch_MatchName(String matchLink, String matchName);
+
+    List<MatchPlayer> findAllByMatch_MatchNameAndMatch_MatchRound(String matchName, Integer matchRound);
+
+    List<MatchPlayer> findAllByMatch_Id(Long matchId);
 }
