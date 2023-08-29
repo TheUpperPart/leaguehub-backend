c9d149398c78af88e1611e681c98b193586c9d4a
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-08-29 19:45:18 +0900

    feat: MatchPlayer 예외 구현

--- src/main/java/leaguehub/leaguehubbackend/exception/match/MatchExceptionCode.java
+++ src/main/java/leaguehub/leaguehubbackend/exception/match/MatchExceptionCode.java
@@ -14,7 +14,8 @@ public enum MatchExceptionCode implements ExceptionCode {
 
     MATCH_NOT_FOUND(NOT_FOUND, "MA-C-001", "유효하지 않은 경기입니다."),
     MATCH_RESULT_NOT_FOUNT(NOT_FOUND, "MA-C-002", "매치 결과를 찾을 수 없습니다."),
-    MATCH_NOT_ENOUGH_PLAYER(BAD_REQUEST, "MA-C-003", "매치 인원수가 충분하지 않습니다.");
+    MATCH_NOT_ENOUGH_PLAYER(BAD_REQUEST, "MA-C-003", "매치 인원수가 충분하지 않습니다."),
+    MATCH_PLAYER_NOT_FOUND(NOT_FOUND, "MA-C-004", "매치 플레이어를 찾을 수 없습니다.");
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
