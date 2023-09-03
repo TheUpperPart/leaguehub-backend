2bc4893bc60701c592cb55f83c1f08192a38b5f8
SungWoo <sungwoo166@gmail.com>
2023-09-03 17:57:32 +0900

    Feat : 매치에 대한 사용자 정보를 전달하기 위한 DTO 구현

---
+++ src/main/java/leaguehub/leaguehubbackend/dto/match/MatchScoreInfoDto.java
@@ -0,0 +1,14 @@
+package leaguehub.leaguehubbackend.dto.match;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class MatchScoreInfoDto {
+    private String requestMatchPlayerId;
+
+    private List<MatchPlayerScoreInfo> matchPlayerScoreInfos;
+}
