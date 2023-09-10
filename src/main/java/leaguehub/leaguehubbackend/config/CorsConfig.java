86536a52fe13d694b8f3698285d7d54b6cd30c27
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-09-10 17:00:28 +0900

    config: cors 및 로컬 운영 프로퍼티 분리

--- src/main/java/leaguehub/leaguehubbackend/config/CorsConfig.java
+++ src/main/java/leaguehub/leaguehubbackend/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true); //내서버가 응답을 할때 json을 자바스크립트에서 처리할 수 있게 할지
-        config.addAllowedOriginPattern("http://localhost:3000"); //모든 아이피를 응답허용
+        config.addAllowedOriginPattern("*"); //모든 아이피를 응답허용
         config.addAllowedHeader("*"); //모든 header 응답허용
         //config.addExposedHeader("*");
         config.addAllowedMethod("*"); //모든 post,get,put 허용
