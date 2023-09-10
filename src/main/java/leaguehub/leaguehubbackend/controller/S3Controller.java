ca542536357bf3b05704e0deb45eee70b8fcdfce
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-09-10 15:43:35 +0900

    docs: Swagger 어노테이션 추가

--- src/main/java/leaguehub/leaguehubbackend/controller/S3Controller.java
+++ src/main/java/leaguehub/leaguehubbackend/controller/S3Controller.java
@@ -1,5 +1,14 @@
 package leaguehub.leaguehubbackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import leaguehub.leaguehubbackend.dto.match.MatchRoundListDto;
+import leaguehub.leaguehubbackend.exception.global.ExceptionResponse;
 import leaguehub.leaguehubbackend.service.s3.S3FileUploadService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +21,7 @@ import org.springframework.web.multipart.MultipartFile;
 import static org.springframework.http.HttpStatus.OK;
 
 @Slf4j
+@Tag(name = "S3-Controller", description = "사진 업로드 관련 API")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -19,6 +29,12 @@ public class S3Controller {
 
     final S3FileUploadService s3FileUploadService;
 
+    @Operation(summary = "사진 업로드 API")
+    @Parameter(name = "multipartFile", description = "사진 파일", example = "image.jpg")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "사진 저장 주소 반환"),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 이미지입니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ExceptionResponse.class)))
+    })
     @PostMapping("/image")
     public ResponseEntity saveProfile(MultipartFile multipartFile) {
         String imageUrl = s3FileUploadService.uploadFile(multipartFile);
