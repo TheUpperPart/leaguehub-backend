b49f4c5fd165b3f3f385e3bd46957a99a1567b37
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-09-14 18:07:40 +0900

    feat: 게임 결과를 저장하는 엔티티 추가 및 몽고 config 설정, 몽고 DAO 생성

---
+++ src/main/java/leaguehub/leaguehubbackend/entity/match/GameResult.java
@@ -0,0 +1,29 @@
+package leaguehub.leaguehubbackend.entity.match;
+
+import jakarta.persistence.Id;
+import leaguehub.leaguehubbackend.dto.match.MatchRankResultDto;
+import leaguehub.leaguehubbackend.entity.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "game_result")
+@Getter
+@NoArgsConstructor
+public class GameResult extends BaseTimeEntity {
+
+    @Id
+    private Long id;
+
+    private List<MatchRankResultDto> matchRankResult;
+
+    public static GameResult createGameResult(Long id, List<MatchRankResultDto> matchRankResult) {
+        GameResult gameResult = new GameResult();
+        gameResult.id = id;
+        gameResult.matchRankResult = matchRankResult;
+
+        return gameResult;
+    }
+}
