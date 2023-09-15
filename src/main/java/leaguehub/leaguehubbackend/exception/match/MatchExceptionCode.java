08621a8adbd38cde379a202dd53cf15c1cd6ec08
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-09-15 19:15:28 +0900

    exception: 이전 경기가 끝나지 않았을 때의 예외처리 구현

--- src/main/java/leaguehub/leaguehubbackend/exception/match/MatchExceptionCode.java
+++ src/main/java/leaguehub/leaguehubbackend/exception/match/MatchExceptionCode.java
@@ -16,7 +16,8 @@ public enum MatchExceptionCode implements ExceptionCode {
     MATCH_RESULT_NOT_FOUNT(NOT_FOUND, "MA-C-002", "매치 결과를 찾을 수 없습니다."),
     MATCH_NOT_ENOUGH_PLAYER(BAD_REQUEST, "MA-C-003", "매치 인원수가 충분하지 않습니다."),
     MATCH_ALREADY_UPDATE(BAD_REQUEST, "MA-C-004", "이미 매치의 점수가 업데이트 되었습니다."),
-    MATCH_PLAYER_NOT_FOUND(NOT_FOUND, "MA-C-005", "해당 매치 플레이어가 없습니다.");
+    MATCH_PLAYER_NOT_FOUND(NOT_FOUND, "MA-C-005", "해당 매치 플레이어가 없습니다."),
+    MATCH_NOT_END(BAD_REQUEST, "MA-C-006", "이전 경기가 끝나지 않았습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
