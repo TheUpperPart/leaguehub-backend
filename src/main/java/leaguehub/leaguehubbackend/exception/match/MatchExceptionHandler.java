c81d1106908b799abedde8e95ff09b1c6fd7c33a
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-09-15 19:06:07 +0900

    Revert "exception: 이전 경기가 끝나지 않았을 때의 예외처리 추가"
    This reverts commit c32ed11913be4df6f7e3b94133aa37324c64e276.

--- src/main/java/leaguehub/leaguehubbackend/exception/match/MatchExceptionHandler.java
+++ src/main/java/leaguehub/leaguehubbackend/exception/match/MatchExceptionHandler.java
@@ -2,7 +2,10 @@ package leaguehub.leaguehubbackend.exception.match;
 
 import leaguehub.leaguehubbackend.exception.global.ExceptionCode;
 import leaguehub.leaguehubbackend.exception.global.ExceptionResponse;
-import leaguehub.leaguehubbackend.exception.match.exception.*;
+import leaguehub.leaguehubbackend.exception.match.exception.MatchAlreadyUpdateException;
+import leaguehub.leaguehubbackend.exception.match.exception.MatchNotEnoughPlayerException;
+import leaguehub.leaguehubbackend.exception.match.exception.MatchNotFoundException;
+import leaguehub.leaguehubbackend.exception.match.exception.MatchResultIdNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -56,19 +59,6 @@ public class MatchExceptionHandler {
     @ExceptionHandler(MatchAlreadyUpdateException.class)
     public ResponseEntity<ExceptionResponse> matchAlreadyUpdateException(
             MatchAlreadyUpdateException e
-    ) {
-        ExceptionCode exceptionCode = e.getExceptionCode();
-        log.error("{}", exceptionCode.getMessage());
-
-        return new ResponseEntity<>(
-                new ExceptionResponse(exceptionCode),
-                exceptionCode.getHttpStatus()
-        );
-    }
-
-    @ExceptionHandler(MatchNotEndException.class)
-    public ResponseEntity<ExceptionResponse> MatchNotEndException(
-            MatchNotEndException e
     ) {
         ExceptionCode exceptionCode = e.getExceptionCode();
         log.error("{}", exceptionCode.getMessage());
