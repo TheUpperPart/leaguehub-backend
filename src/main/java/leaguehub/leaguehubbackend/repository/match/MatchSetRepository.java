c40af764969eb4fecd4169b33c0bb74261fae7db
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-09-16 21:54:54 +0900

    feat: Repository 코드 수정 및 추가

--- src/main/java/leaguehub/leaguehubbackend/repository/match/MatchSetRepository.java
+++ src/main/java/leaguehub/leaguehubbackend/repository/match/MatchSetRepository.java
@@ -3,8 +3,11 @@ package leaguehub.leaguehubbackend.repository.match;
 import leaguehub.leaguehubbackend.entity.match.MatchSet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MatchSetRepository extends JpaRepository<MatchSet, Long> {
     Optional<MatchSet> findMatchSetByMatchIdAndAndSetCount(Long matchId, Integer setCount);
+
+    List<MatchSet> findMatchSetsByMatch_Id(Long matchId);
 }
