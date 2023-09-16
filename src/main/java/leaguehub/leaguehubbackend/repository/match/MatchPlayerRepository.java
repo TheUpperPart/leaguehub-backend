512627dd8b436979a884e97a5b851c213aa5b63c
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-09-16 22:06:07 +0900

    feat: 참가자 실격 처리시 관련 매치 상태 전부 실격 처리 기능 구현

--- src/main/java/leaguehub/leaguehubbackend/repository/match/MatchPlayerRepository.java
+++ src/main/java/leaguehub/leaguehubbackend/repository/match/MatchPlayerRepository.java
@@ -28,4 +28,6 @@ public interface MatchPlayerRepository extends JpaRepository<MatchPlayer, Long> 
 
     Optional<MatchPlayer> findByParticipantIdAndMatchId(Long participantId, Long matchId);
 
+    List<MatchPlayer> findMatchPlayersByParticipantId(Long participantId);
+
 }
