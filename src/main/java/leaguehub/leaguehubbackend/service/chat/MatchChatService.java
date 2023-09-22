f852fa3f66f794dcc1a54599e6065ac7854bad0d
홍성우 <sungwoo166@gmail.com>
2023-09-23 08:47:58 +0900

    Feat#307 : 리그(채널) 종료시 채팅 삭제 (#309)

--- src/main/java/leaguehub/leaguehubbackend/service/chat/MatchChatService.java
+++ src/main/java/leaguehub/leaguehubbackend/service/chat/MatchChatService.java
@@ -4,6 +4,7 @@ package leaguehub.leaguehubbackend.service.chat;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import leaguehub.leaguehubbackend.dto.chat.MatchMessage;
+import leaguehub.leaguehubbackend.entity.channel.Channel;
 import leaguehub.leaguehubbackend.exception.chat.exception.MatchChatMessageConversionException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,8 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Slf4j
@@ -22,6 +25,7 @@ public class MatchChatService {
     private final ObjectMapper objectMapper;
     private static final String REDIS_KEY_FORMAT = "channelId:%d:matchId:%d:messages";
     private static final String PUBLISH_KEY_FORMAT = "matchId:%d:messages";
+    private static final String DELETE_CHANNEL_CHAT_FORMAT = "channelId:%d:matchId:*:messages";
 
     public void processMessage(MatchMessage message) {
 
@@ -53,5 +57,17 @@ public class MatchChatService {
 
     private void publishMessage(String key, String messageJson) {
         stringRedisTemplate.convertAndSend(key, messageJson);
+    }
+
+    public void deleteChannelMatchChat(Channel channel) {
+
+        String targetChannel = String.format(DELETE_CHANNEL_CHAT_FORMAT, channel.getId());
+        Set<String> keys = stringRedisTemplate.keys(targetChannel);
+
+        if (keys != null) {
+            for (String key : keys) {
+                stringRedisTemplate.delete(key);
+            }
+        }
     }
 }
