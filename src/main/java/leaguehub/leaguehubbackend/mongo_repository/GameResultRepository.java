8e6a04389657a814a216017bbc492af5a3b21232
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-09-25 18:37:53 +0900

    fix: Mongo custom Id 사용

--- src/main/java/leaguehub/leaguehubbackend/mongo_repository/GameResultRepository.java
+++ src/main/java/leaguehub/leaguehubbackend/mongo_repository/GameResultRepository.java
@@ -3,6 +3,8 @@ package leaguehub.leaguehubbackend.mongo_repository;
 import leaguehub.leaguehubbackend.entity.match.GameResult;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface GameResultRepository extends MongoRepository<GameResult, Long> {
+import java.util.Optional;
 
+public interface GameResultRepository extends MongoRepository<GameResult, Long> {
+    Optional<GameResult> findGameResultById(Long id);
 }
