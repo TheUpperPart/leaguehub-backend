7d33b82e5df9ca1bba30a5a5c3485f28eac8a1c1
홍성우 <sungwoo166@gmail.com>
2023-10-01 08:24:38 +0900

    Refactor#335 : MatchPlayerScoreInfoDto Refactor (#336)

--- src/main/java/leaguehub/leaguehubbackend/dto/match/MatchScoreInfoDto.java
+++ src/main/java/leaguehub/leaguehubbackend/dto/match/MatchScoreInfoDto.java
@@ -10,5 +10,5 @@ import java.util.List;
 public class MatchScoreInfoDto {
     private String requestMatchPlayerId;
 
-    private List<MatchPlayerScoreInfo> matchPlayerScoreInfos;
+    private List<MatchPlayerInfo> matchPlayerInfos;
 }
