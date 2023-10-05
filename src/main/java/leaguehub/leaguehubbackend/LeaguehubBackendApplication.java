1eb5b9f9b79458fc07bf577d6ef3387ba4817022
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2023-10-05 22:20:56 +0900

    Refactor: MongoDB 삭제

--- src/main/java/leaguehub/leaguehubbackend/LeaguehubBackendApplication.java
+++ src/main/java/leaguehub/leaguehubbackend/LeaguehubBackendApplication.java
@@ -1,16 +1,11 @@
 package leaguehub.leaguehubbackend;
 
-import jakarta.annotation.PostConstruct;
-import leaguehub.leaguehubbackend.util.UserUtil;
-import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "leaguehub.leaguehubbackend.repository")
-@EnableMongoRepositories(basePackages = "leaguehub.leaguehubbackend.mongo_repository")
 public class LeaguehubBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(LeaguehubBackendApplication.class, args);
