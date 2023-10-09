4ce20e1e434132c0d92eece10772f6930b82cfec
TinyFrogs <87762815+TinyFrogs@users.noreply.github.com>
2023-10-09 19:46:55 +0900

    Refactor#365: 라운드 배정 시 matchStatus 상태 변경 추가 (#369)

--- src/main/java/leaguehub/leaguehubbackend/service/match/MatchService.java
+++ src/main/java/leaguehub/leaguehubbackend/service/match/MatchService.java
@@ -314,6 +314,8 @@ public class MatchService {
                 playerIndex++;
                 remainingPlayers--;
             }
+
+            match.updateMatchStatus(MatchStatus.PROGRESS);
         }
     }
 
