24482f0e35f01a6cd40987f75f322233ca62e9a7
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-10-09 18:22:56 +0900

    refactor: 매치 배정시 매치 상태 변경 추가

--- src/main/java/leaguehub/leaguehubbackend/service/match/MatchService.java
+++ src/main/java/leaguehub/leaguehubbackend/service/match/MatchService.java
@@ -305,6 +305,8 @@ public class MatchService {
                 playerIndex++;
                 remainingPlayers--;
             }
+
+            match.updateMatchStatus(MatchStatus.PROGRESS);
         }
     }
 
