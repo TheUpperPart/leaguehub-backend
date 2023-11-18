22d8df2d6cecfcb1a73595388b193cdc5ecd406a
spiky-haired-Pyramid-head <qns0147@gmail.com>
2023-11-18 20:47:36 +0900

    feat: Channel 생성시 ChannelInfo도 같이 생성하도록 수정

---
+++ src/main/java/leaguehub/leaguehubbackend/repository/channel/ChannelInfoRepository.java
@@ -0,0 +1,7 @@
+package leaguehub.leaguehubbackend.repository.channel;
+
+import leaguehub.leaguehubbackend.entity.channel.ChannelInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChannelInfoRepository extends JpaRepository<ChannelInfo, Long> {
+}
