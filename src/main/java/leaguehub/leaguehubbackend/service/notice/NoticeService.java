160f125cacb9a0fccb2f478a69d9013c0756b681
SungWoo <sungwoo166@gmail.com>
2023-11-27 17:09:18 +0900

    feat: 하스스톤, fc, 롤 공지 스크래핑 기능 추가

--- src/main/java/leaguehub/leaguehubbackend/service/notice/NoticeService.java
+++ src/main/java/leaguehub/leaguehubbackend/service/notice/NoticeService.java
@@ -2,26 +2,77 @@ package leaguehub.leaguehubbackend.service.notice;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import leaguehub.leaguehubbackend.dto.notice.Notice;
+import leaguehub.leaguehubbackend.exception.notice.exception.NoticeUnsupportedException;
+import leaguehub.leaguehubbackend.exception.notice.exception.WebScrapingException;
 import lombok.RequiredArgsConstructor;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 
 @Service
 @RequiredArgsConstructor
 public class NoticeService {
+    private static final String TFT_URL = "https://www.leagueoflegends.com/ko-kr/news/game-updates/";
+    private static final String TFT_SELECTOR = "#gatsby-focus-wrapper > div > div.style__Wrapper-sc-1ynvx8h-0.style__ResponsiveWrapper-sc-1ynvx8h-6.bNRNtU.dzWqHp > div > div.style__Wrapper-sc-106zuld-0.style__ResponsiveWrapper-sc-106zuld-4.enQqER.jYHLfd.style__List-sc-1ynvx8h-3.qfKFn > div > ol > li";
+    private static final String TFT_TITLE_SELECTOR = "a > article > div.style__Info-sc-1h41bzo-6.eBtwVi > div > h2";
+    private static final String LOL_URL = "https://www.leagueoflegends.com/ko-kr/news/notices/";
+    private static final String LOL_SELECTOR = "#gatsby-focus-wrapper > div > div.style__Wrapper-sc-1ynvx8h-0.style__ResponsiveWrapper-sc-1ynvx8h-6.bNRNtU.dzWqHp > div > div.style__Wrapper-sc-106zuld-0.style__ResponsiveWrapper-sc-106zuld-4.enQqER.jYHLfd.style__List-sc-1ynvx8h-3.qfKFn > div > ol > li";
+    private static final String LOL_TITLE_SELECTOR = "a > article > div.style__Info-sc-1h41bzo-6.eBtwVi > div > h2";
+    private static final String FC_URL = "https://fconline.nexon.com/news/notice/list";
+    private static final String FC_SELECTOR = "#divListPart > div.board_list > div.content > div.list_wrap > div.tbody > div:nth-child(%d) > a";
+    private static final String FC_TITLE_SELECTOR = "a > span.td.subject";
+    private static final String HOS_URL = "https://news.blizzard.com/ko-kr/hearthstone";
+    private static final String HOS_SELECTOR = "#recent-articles > li:nth-child(%d) > article > a";
 
-    public List<Notice> getNotice(String target) {
-        if ("tft".equals(target)) {
-            return scrapeTftNotice();
-        } else {
-            return getLeagueHubNotice();
+    public List<Notice> getNotice(String target) throws NoticeUnsupportedException {
+        return switch (target) {
+            case "tft" -> scrapeNotices(TFT_URL, TFT_SELECTOR, TFT_TITLE_SELECTOR, null, null);
+            case "lol" -> scrapeNotices(LOL_URL, LOL_SELECTOR, LOL_TITLE_SELECTOR, null, null);
+            case "fc" -> scrapeNotices(FC_URL, FC_SELECTOR, FC_TITLE_SELECTOR, 4, 9);
+            case "hos" -> scrapeNotices(HOS_URL, HOS_SELECTOR, null, 1, 6);
+            case "main" -> getLeagueHubNotice();
+            default -> throw new NoticeUnsupportedException();
+        };
+    }
+
+    private List<Notice> scrapeNotices(String url, String itemSelector, String titleSelector, Integer start, Integer end) throws WebScrapingException {
+        List<Notice> notices = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(url).get();
+
+            int startIndex = start != null ? start : 1;
+            int endIndex = end != null ? end : doc.select(itemSelector).size();
+
+            return IntStream.rangeClosed(startIndex, endIndex)
+                    .mapToObj(i -> doc.select(String.format(itemSelector, i)))
+                    .filter(elements -> !elements.isEmpty())
+                    .map(Elements::first).filter(Objects::nonNull)
+                    .map(newsItem ->
+                            createNoticeFromElement(newsItem, titleSelector))
+                    .collect(Collectors.toList());
+
+        } catch (Exception e) {
+            throw new WebScrapingException();
         }
     }
+
+    private Notice createNoticeFromElement(Element element, String titleSelector) {
+        String newsLink = element.select("a").attr("abs:href");
+        String title = titleSelector != null ? element.select(titleSelector).text() : element.text();
+
+        return Notice.builder()
+                .noticeLink(newsLink)
+                .noticeTitle(title)
+                .build();
+    }
+
     private List<Notice> getLeagueHubNotice() {
         List<Notice> notices = new ArrayList<>();
 
@@ -37,35 +88,5 @@ public class NoticeService {
                 .noticeTitle(title)
                 .noticeInfo(info)
                 .build();
-    }
-
-
-    private List<Notice> scrapeTftNotice() {
-        List<Notice> notices = new ArrayList<>();
-        try {
-            Document doc = Jsoup.connect("https://www.leagueoflegends.com/ko-kr/news/game-updates/").get();
-
-            Elements newsItems = doc.select(
-                    "#gatsby-focus-wrapper > div > div.style__Wrapper-sc-1ynvx8h-0.style__ResponsiveWrapper-sc-1ynvx8h-6.bNRNtU.dzWqHp > div > div.style__Wrapper-sc-106zuld-0.style__ResponsiveWrapper-sc-106zuld-4.enQqER.jYHLfd.style__List-sc-1ynvx8h-3.qfKFn > div > ol > li");
-
-            for (Element item : newsItems) {
-                String newsLink = item.select("a").attr("abs:href");
-                String title = item.select("a > article > div.style__Info-sc-1h41bzo-6.eBtwVi > div > h2").text();
-                String metaData = item.select(
-                                "a > article > div.style__Info-sc-1h41bzo-6.eBtwVi > div > div.style__Meta-sc-1h41bzo-10.hGstqB")
-                        .text();
-
-                Notice notice = Notice.builder()
-                        .noticeLink(newsLink)
-                        .noticeTitle(title)
-                        .noticeInfo(metaData)
-                        .build();
-
-                notices.add(notice);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return notices;
     }
 }
