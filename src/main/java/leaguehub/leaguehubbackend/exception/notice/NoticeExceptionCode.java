6b22c028727710ab0e5cae96b73bf8f506708bc3
SungWoo <sungwoo166@gmail.com>
2023-11-27 17:08:41 +0900

    feat: 게임 공지 스크래핑 에러코드 추가

---
+++ src/main/java/leaguehub/leaguehubbackend/exception/notice/NoticeExceptionCode.java
@@ -0,0 +1,21 @@
+package leaguehub.leaguehubbackend.exception.notice;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import leaguehub.leaguehubbackend.exception.global.ExceptionCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum NoticeExceptionCode implements ExceptionCode {
+
+    NOTICE_UNSUPPORTED(BAD_REQUEST, "NT-C-002", "지원되지 않는 공지사항 기능입니다."),
+    WEB_SCRAPING_ERROR(BAD_REQUEST, "WS-C-001", "웹 페이지에서 정보를 추출하는 과정에서 오류가 발생했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
