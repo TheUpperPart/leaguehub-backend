c6d2213add4ccef7ce9aabfae8e56dc22263f9ea
spiky-haired-Pyramid-head <qns0147@gmail.com>
2024-01-11 15:32:22 +0900

    refactor: 불필요한 @EnableJpaRepositories 어노테이션 삭제

--- src/main/java/leaguehub/leaguehubbackend/LeaguehubBackendApplication.java
+++ src/main/java/leaguehub/leaguehubbackend/LeaguehubBackendApplication.java
@@ -2,10 +2,8 @@ package leaguehub.leaguehubbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "leaguehub.leaguehubbackend.repository")
 public class LeaguehubBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(LeaguehubBackendApplication.class, args);
