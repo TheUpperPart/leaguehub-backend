1008a8d73136f8daa4275aada78d8de1b9b9ab83
SungWoo Hong <>
2024-01-11 23:47:36 +0900

    refactor: Member Entity와 MembmerRepository 에 refreshToken 관련코드 삭제

--- src/main/java/leaguehub/leaguehubbackend/repository/member/MemberRepository.java
+++ src/main/java/leaguehub/leaguehubbackend/repository/member/MemberRepository.java
@@ -10,10 +10,8 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-
     Optional<Member> findMemberByPersonalId(String personalId);
     Optional<Member> findByNickname(String nickname);
-    Optional<Member> findByRefreshToken(String refreshToken);
     @Query("SELECT m FROM Member m JOIN m.emailAuth e WHERE e.email = :email")
     Optional<Member> findMemberByEmail(@Param("email") String email);
     @Query("SELECT m FROM Member m WHERE m.emailAuth = :emailAuth")
